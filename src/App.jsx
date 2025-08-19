08d556d337455e6195d01a07686525a52b58926a
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2025-08-19 17:47:39 +0500

    create login form and aslo define validation with yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Login from "./components/Auth/Login";
 
 function App() {
-  return <div>Main App Page</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 function Login() {
-  return <div></div>;
+  const loginSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(loginSchema), // ✅ yup schema connected
+  });
+
+  const onSubmit = (data) => {
+    console.log("Data", data);
+    reset();
+  };
+  return (
+    <div className="w-full h-screen flex justify-center items-center px-2">
+      <div className="w-full max-w-[500px]  border-emerald-300 border-2 p-6 md:p-12 rounded-3xl">
+        <form
+          action=""
+          className="flex flex-col gap-8"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="w-full flex flex-col gap-2">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full border-emerald-300 border-2 py-3 rounded-3xl outline-none text-gray-300 placeholder:text-gray-300 p-4  text-xl bg-transparent"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="w-full border-emerald-300 border-2 py-3 rounded-3xl outline-none text-gray-300 placeholder:text-gray-300 p-4 bg-transparent text-xl"
+              {...register("password")}
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+          </div>
+          <div className="w-full">
+            <button className="w-full py-2 bg-green-600 hover:bg-green-500 transition-all duration-100  text-2xl text-white rounded-3xl cursor-pointer">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
